45629c2011739fb7aa13782f9d7a03c82a5a1575
jtplatt99 <jonathantplatt@gmail.com>
2017-11-12 13:27:06 -0600

    Create prog05.f95

---
+++ prog05.f95
@@ -0,0 +1,22 @@
+!-------------------------------------------------
+! This program will convert take a user given
+! integer speed (mph) and return whether the user
+! is speeding or not 
+!
+! Written by Jonathan Platt Section 1 11/12/2017
+!-------------------------------------------------
+PROGRAM prog05
+  IMPLICIT NONE
+  INTEGER::speed,speedLimit=55
+  WRITE(*,*) "Please enter your speed as an INTEGER in miles per hour:"
+  READ*,speed
+  IF (speed<speedLimit) THEN
+    WRITE(*,*) "With a speed of",speed,"this vehicle is not speeding."
+  ELSE IF (speed>speedLimit) THEN
+    WRITE(*,*) "With a speed of",speed,"this vehicle is speeding."
+  ELSE IF (speed=speedLimit) THEN
+    WRITE(*,*) "With a speed of",speed,"this vehicle is really pushing it."
+  ELSE 
+    PRINT*,"SOMETHING WENT WRONG"
+  END IF
+END PROGRAM prog05
